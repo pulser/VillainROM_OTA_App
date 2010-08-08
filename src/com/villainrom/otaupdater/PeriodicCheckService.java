6b5123918bed15bddffdba6b417ee2eb6e0e55ae
Antti S. Lankila <alankila@gmail.com>
2010-08-08 18:01:07 +0300

    Add log for start/stop.

--- src/com/villainrom/otaupdater/PeriodicCheckService.java
+++ src/com/villainrom/otaupdater/PeriodicCheckService.java
@@ -3,6 +3,7 @@ package com.villainrom.otaupdater;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * This service accesses the VillainROM servers to discover new updates,
@@ -11,10 +12,23 @@ import android.os.IBinder;
  * @author alankila
  */
 public class PeriodicCheckService extends Service {
+	private static final String TAG = PeriodicCheckService.class.getSimpleName();
+
 	/** You can't connect to this service. */
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.i(TAG, "Starting");
+	}
 
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(TAG, "Stopping");
+	}
 }
