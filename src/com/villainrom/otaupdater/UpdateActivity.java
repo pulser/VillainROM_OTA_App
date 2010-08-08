d7a799866cfb1a6331755eb4c0328560315aa201
pulser <pulser@villainrom.co.uk>
2010-08-08 15:08:08 +0100

    Created Eclipse Project

---
+++ src/com/villainrom/otaupdater/UpdateActivity.java
@@ -0,0 +1,13 @@
+package com.villainrom.otaupdater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UpdateActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
